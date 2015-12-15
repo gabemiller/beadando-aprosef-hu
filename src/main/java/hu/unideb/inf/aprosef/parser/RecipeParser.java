e40a50c641363885ebb44cc81d12184e4214044c
Gabor Molnar <gabormolnar92@gmail.com>
2015-12-15 12:54:54 +0100

    RecipeParser
    Create parse method
    Add logback
    Add config

--- src/main/java/hu/unideb/inf/aprosef/parser/RecipeParser.java
+++ src/main/java/hu/unideb/inf/aprosef/parser/RecipeParser.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import hu.unideb.inf.aprosef.model.Recipe;
 import hu.unideb.inf.aprosef.model.Recipe.Author;
@@ -16,6 +18,8 @@ import hu.unideb.inf.aprosef.model.Recipe.Instruction;
 import hu.unideb.inf.jaxb.JAXBUtil;
 
 public class RecipeParser {
+
+	private static Logger logger = LoggerFactory.getLogger(RecipeParser.class);
 
 	public RecipeParser() {
 	}
@@ -39,16 +43,17 @@ public class RecipeParser {
 		String image = null;
 
 		try {
-			image = doc.select("div#field_mainphoto > a").attr("href");
+			image = doc.select("div#field_mainphoto > a").first().attr("href");
+			logger.info("Image: {}", image);
 		} catch (Exception e) {
 			throw new IOException("Malformed document");
 		}
 		recipe.setImage(image);
 
 		String title = null;
-
 		try {
 			title = doc.select("h1.recipetitle").first().text().trim();
+			logger.info("Title: {}", title);
 		} catch (Exception e) {
 			throw new IOException("Malformed document");
 		}
@@ -58,14 +63,16 @@ public class RecipeParser {
 		try {
 			author.setName(doc.select("div.username > a > span").first().text().trim());
 			author.setProfile(doc.select("div.username > a").attr("href"));
+			logger.info("Author: {} {}", author.getName(), author.getProfile());
 		} catch (Exception e) {
 			throw new IOException("Malformed document");
 		}
 		recipe.setAuthor(author);
-		
+
 		Integer yield = null;
 		try {
 			yield = Integer.parseInt(doc.select("div[itemprop=recipeYield] > span#number").first().text().trim());
+			logger.info("Yield: {}", yield);
 		} catch (Exception e) {
 			throw new IOException("Malformed document");
 		}
@@ -73,22 +80,49 @@ public class RecipeParser {
 
 		List<Ingredient> ingredients = new ArrayList<Ingredient>();
 		try {
-			for (Element e : doc.select("div[itemprop=recipeInstructions] > div.step")) {
+			for (Element e : doc.select("div.field_counted_ingredients > ul > li")) {
 				Ingredient ing = new Ingredient();
-				
-				
-				
+
+				if (!e.select("span").isEmpty()) {
+					ing.setAmount(Double.parseDouble(e.select("span").first().text().trim()));
+				} else {
+					logger.warn("This ingredient does not have amount value!");
+					ing.setAmount(null);
+				}
+
+				if (!e.select("a").isEmpty()) {
+					ing.setName(e.select("a").first().text().trim());
+					if (!e.ownText().isEmpty()){
+						ing.setUnit(e.ownText());
+					}else{
+						logger.warn("This ingredient does not have unit value!");
+						ing.setUnit(null);
+					}
+				} else {
+					String[] ownText = e.ownText().split(" ", 1);
+
+					if (ownText.length == 2) {
+						ing.setUnit(ownText[0]);
+						ing.setName(ownText[1]);
+					} else {
+						ing.setName(ownText[0]);
+					}
+				}
+
+				logger.info("Ingredient: {}", ing);
+
 				ingredients.add(ing);
 			}
 		} catch (Exception e) {
 			throw new IOException("Malformed document");
 		}
 		recipe.setIngredients(ingredients);
-		
+
 		Integer totalTime = null;
-		
+
 		try {
 			totalTime = Integer.parseInt(doc.select("time[itemprop=totalTime] > span.number").first().text().trim());
+			logger.info("Total time: {}", totalTime);
 		} catch (Exception e) {
 			throw new IOException("Malformed document");
 		}
@@ -97,6 +131,7 @@ public class RecipeParser {
 		String description = null;
 		try {
 			description = doc.select("div#summary > span[itemprop=description]").first().text().trim();
+			logger.info("Description: {}", description);
 		} catch (Exception e) {
 			throw new IOException("Malformed document");
 		}
@@ -108,14 +143,16 @@ public class RecipeParser {
 				Instruction ins = new Instruction();
 				ins.setStep(Integer.parseInt(e.select("div.stepnumber").first().text().trim()));
 				ins.setText(e.select("div.text").first().text().trim());
-				
+
+				logger.info("Instruction: {}", ins);
+
 				instructions.add(ins);
 			}
 		} catch (Exception e) {
 			throw new IOException("Malformed document");
 		}
 		recipe.setInstructions(instructions);
-		
+
 		return recipe;
 	}
 
